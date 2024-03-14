e4a7052f37cd214c6a0f2723bf9536de184924a6
Shuaicong Wu <niklaus.fangtasia@gmail.com>
2024-03-14 23:40:25 +0100

    add a new function with the same functionality

--- statistics_refer-yt.py
+++ statistics_refer-yt.py
@@ -32,10 +32,13 @@ def process_json_file(file_path, video_expression_dict):
 
 video_expression_dict = {}
 
-video_expression_dict = process_json_file('data/Ref-YT/meta_expressions/test/meta_expressions.json', video_expression_dict)
-video_expression_dict = process_json_file('data/Ref-YT/meta_expressions/train/meta_expressions.json', video_expression_dict)
-video_expression_dict = process_json_file('data/Ref-YT/meta_expressions/valid/meta_expressions.json', video_expression_dict)
-print(f"Total length of expression_dict: {len(video_expression_dict)}")   # 3978
+video_expression_dict = process_json_file('data/Ref-YT/meta_expressions/test/meta_expressions.json',
+                                          video_expression_dict)
+video_expression_dict = process_json_file('data/Ref-YT/meta_expressions/train/meta_expressions.json',
+                                          video_expression_dict)
+video_expression_dict = process_json_file('data/Ref-YT/meta_expressions/valid/meta_expressions.json',
+                                          video_expression_dict)
+print(f"Total length of expression_dict: {len(video_expression_dict)}")  # 3978
 
 for video_id, expressions in video_expression_dict.items():
     # print(f"Video ID: {video_id}")
@@ -44,7 +47,30 @@ for video_id, expressions in video_expression_dict.items():
         # print(f"- {expression}")
         unique_expression_set.update([expression])
 
-# print(f"Expressions: {unique_expression_set}")
+print(f"Expressions: {len(unique_expression_set)}")  # 14289
+
+# Both functions work for calculating the number of queries!
+paths = ['data/Ref-YT/meta_expressions/test/meta_expressions.json',
+         'data/Ref-YT/meta_expressions/train/meta_expressions.json',
+         'data/Ref-YT/meta_expressions/valid/meta_expressions.json']
+
+
+def get_all_expressions(file_paths):
+    unique_queries_set = set()
+    for file_path in file_paths:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+
+        for video_id, video_data in data.get('videos', {}).items():
+            expressions = video_data.get('expressions', {})
+            for expression_id, expression_data in expressions.items():
+                expression = expression_data.get('exp', '')
+                unique_queries_set.add(expression)
+    return unique_queries_set
+
+
+unique_queries = get_all_expressions(paths)
+print(f'There are in total {len(unique_queries)} different kinds of queries.')  # 634
 
 
 def get_verb_and_frequency_from_sentences(sentences):
@@ -78,5 +104,4 @@ def get_verb_and_frequency_from_sentences(sentences):
 
     print("Data saved to:", output_file_path)
 
-
-get_verb_and_frequency_from_sentences(unique_expression_set)
+# get_verb_and_frequency_from_sentences(unique_expression_set)
