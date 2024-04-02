aa6726ef0b7c4b4b1de21cb88a15a5e13fc5082e
Shuaicong Wu <niklaus.fangtasia@gmail.com>
2024-04-03 01:09:04 +0200

    generate gt files for the results.json which is evaluated from DVIS-Plus using ovis_534.pth

---
+++ dvis.py
@@ -0,0 +1,80 @@
+import numpy as np
+from pycocotools import mask as coco_mask
+import json
+import os
+import time
+
+
+def decode_segmentations(segmentations):
+    boxes = []
+    for segment in segmentations:
+        # {segment['segmentations']} is like {'size': [1080, 1920], 'counts': 'PPYo1'}
+        segmentation = segment['segmentations']
+        # Decode RLE format to binary mask
+        mask = coco_mask.decode(segmentation)
+
+        # Ture if in the mask there is at least one non-zero value.
+        if np.any(mask):
+            # Calculate bounding box from mask
+            # NOTE: bbox.size > 0 is always true
+            bbox = np.argwhere(mask)
+            xmin = np.min(bbox[:, 1])
+            xmax = np.max(bbox[:, 1])
+            ymin = np.min(bbox[:, 0])
+            ymax = np.max(bbox[:, 0])
+
+            # Append bounding box coordinates to the list
+            boxes.append([segment['frame'], segment['obj_id'], xmin, ymin, xmax - xmin, ymax - ymin,
+                          segment['score'], 1, 1])
+
+    return boxes
+
+
+def save_to_file(decoded_segmentations, foldername, filename):
+    if not os.path.exists(foldername):
+        os.makedirs(foldername)
+
+    filepath = os.path.join(foldername, filename)
+    with open(filepath, 'w') as f:
+        for segmentation in decoded_segmentations:
+            f.write(' '.join(map(str, segmentation)) + '\n')
+
+
+def process_json_file(json_file):
+    print(f'Program starts!')
+
+    start_time = time.time()
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+
+    video_segmentations = {}
+    obj_id = 1
+
+    for item in data:
+        video_id = item['video_id']
+        segmentations = item['segmentations']
+        score = item['score']
+        if video_id not in video_segmentations:
+            video_segmentations[video_id] = []
+
+        for index, segmentation in enumerate(segmentations):
+            video_segmentations[video_id].append({'frame': index + 1, 'obj_id': obj_id,
+                                                  'segmentations': segmentation, 'score': score})
+        obj_id += 1
+
+        if obj_id > 20:
+            obj_id = 1
+
+    for video_id, segments in video_segmentations.items():
+        decoded_segmentations = decode_segmentations(segments)
+        print(decoded_segmentations)
+        save_to_file(decoded_segmentations, 'data/generated_by_code/dvis_bboxes/', f'{video_id}.txt')
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    # Program completed! Elapsed time: 770.6677641868591 seconds
+    print(f'Program completed! Elapsed time: {elapsed_time} seconds')
+
+
+json_file = 'data/DVIS/output_DVIS_Plus_Offline_VitAdapterL_OVIS/inference/results.json'
+process_json_file(json_file)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import torch
 
 with open('annotations_train.json', 'r') as f:
     data = json.load(f)
@@ -34,7 +35,64 @@ def generate_mot_file():
     print("conversion to gt.txt complete.")
 
 
+def calculate_move():
+    numbers1 = [-1377, -1489, -1201, -928, -1297, -925, -1360, -1116, -1220, -1113, -1284,
+               -1438, -1533, -1585, -1414, -1412, -1603]
+
+    numbers2 = [-1428, -957, -1134, -993, -1079, -1239, -1078, -1220, -1171, -1363, -1438]
+    numbers3 = [-1533, -1585, -1414, -1412, -1603, -1428, -1377, -994, -1347, -1172, -1241]
+    numbers = [-1073, -1253, -1006, -1367, -1363, -1438, -1533, -1585, -1414, -1412, -1603]
+    numbers = [-1377, -994, -1347, -1172, -1241, -1073, -1253, -1006, -1489]
+    # result = [numbers[0] + 1550] + [num + 1560 for num in numbers[1:]]
+    result = [num + 1560 for num in numbers]
+
+    for num in result:
+        print(num)
+
+
+def read_pth_file():
+    ctvis_vitl_ovis = torch.load('data/DVIS/weights/ctvis_vitl_ovis.pth', map_location=torch.device('cpu'))
+    print(f'ctvis_vitl_ovis.pth: {ctvis_vitl_ovis.keys()}')
+    offline_vitl_ovis_534 = torch.load('data/DVIS/weights/offline_vitl_ovis_534.pth', map_location=torch.device('cpu'))
+
+
+def print_keys(d, depth=0):
+    for key, value in d.items():
+        print("  " * depth + str(key))
+        if isinstance(value, dict):
+            print_keys(value, depth + 1)
+
+
+ctvis_vitl_ovis = torch.load('data/DVIS/weights/dinov2_vitl14_pretrain_.pth', map_location=torch.device('cpu'))
+instances_predictions = torch.load('data/DVIS/output_DVIS_Plus_Offline_VitAdapterL_OVIS/inference/instances_predictions.pth', map_location=torch.device('cpu'))
+
+
+def print_json_keys(data, indent=0):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            print("  " * indent + str(key))
+            if isinstance(value, (dict, list)):
+                print_json_keys(value, indent + 1)
+    elif isinstance(data, list):
+        for item in data:
+            print_json_keys(item, indent + 1)
+
+
+def read_json_file(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    return data
+
+
+file_path = 'data/DVIS/output_DVIS_Plus_Offline_VitAdapterL_OVIS/inference/results.json'
+json_data = read_json_file(file_path)
+
+
 if __name__ == '__main__':
-    generate_mot_file()
+    # generate_mot_file()
+    # calculate_move()
+    # read_pth_file()
+    print(instances_predictions)
+    # print_json_keys(json_data)
 
 
